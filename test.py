ede463daf9df12e7d5713a8be935d689ec4f82ff
Zach Williams <hey@zachwill.com>
2011-04-27 17:28:54 -0500

    Added xml attribute test

--- test.py
+++ test.py
@@ -48,6 +48,11 @@ class TestXML2Dict(unittest.TestCase):
         expected_output = {'a': {'b': ['1', '2'], 'c': {'d': '3'}}}
         self.assertEqual(xml2dict(xml), expected_output)
 
+    def test_xml_attributes_retained(self):
+        xml = self.xml + '<numbers one="1" two="2" />'
+        expected_output = {'numbers': {'one': '1', 'two': '2'}}
+        self.assertEqual(xml2dict(xml), expected_output)
+
 
 class TestFredClassInit(unittest.TestCase):
 
