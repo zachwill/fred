943f52a3e86cc3662d7a3d0f1e41591e1fcfdb39
Zach Williams <hey@zachwill.com>
2011-05-23 02:34:11 -0500

    Removed read_file function and added long description

--- setup.py
+++ setup.py
@@ -35,22 +35,104 @@ try:
 except ImportError:
     from distutils.core import setup
 
+long_description = """
+Fred
+====
 
-def read_file(filename):
-    """
-    Reads the contents of a given file relative to the directory
-    containing this file and returns it.
+Python wrapper of the St. Louis Federal Reserve Bank's [FRED API web
+service](http://api.stlouisfed.org/docs/fred/) for retrieving economic data.
 
-    :param filename:
-        The file to open and read contents from.
-    """
-    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+FRED API Documentation:
+[http://api.stlouisfed.org/docs/fred/](http://api.stlouisfed.org/docs/fred/)
 
+Sign up for a FRED API key:
+[http://api.stlouisfed.org/api_key.html](http://api.stlouisfed.org/api_key.html)
+
+
+Usage
+-----
+
+Without your API key saved in the `fred_api_key.py` file:
+
+    >>> from fred import Fred
+    >>> Fred('my_api_key').category(category_id=125)
+
+With your API key saved in the `fred_api_key` file:
+
+    >>> from fred import Fred
+    >>> Fred().category(category_id=125)
+
+
+### Methods
+
+* `category` -- Get economic data for a specific category.
+<pre><code>
+    >>> Fred().category(category_id=120)
+</code></pre>
+
+
+* `releases` -- Get all releases of economic data.
+<pre><code>
+    >>> Fred().releases(limit=10)
+
+    >>> Fred().releases('dates', xml_output=True)
+</code></pre>
+
+
+* `release` -- Get economic data for a specific release.
+<pre><code>
+    >>> Fred().release('series', release_id=51)
+</code></pre>
+
+
+* `series` -- Get economic series of data.
+<pre><code>
+    >>> Fred().series('search', search_text="money stock")
+
+    >>> Fred().series(series_id='IRA')
+</code></pre>
+
+
+* `sources` -- Get all of FRED's sources of economic data.
+<pre><code>
+    >>> Fred().sources()
+</code></pre>
+
+
+* `source` -- Get a single source of economic data.
+<pre><code>
+    >>> Fred().source(source_id=51)
+</code></pre>
+
+
+* `api` -- Generic way of interacting with the FRED API.
+<pre><code>
+    >>> Fred().api('release', 'dates', release_id=51)
+
+    >>> Fred().api('category', category_id=119)
+</code></pre>
+
+
+**NOTE**: Normally, data is returned in dictionary format instead of XML. If you're
+looking for XML output, however, just pass in the `xml_output=True` argument to a
+method.
+
+    >>> Fred().releases(xml_output=True)
+
+
+License
+-------
+
+**Author**: Zach Williams
+
+All code released under [the Unlicense](http://unlicense.org/) (a.k.a. Public
+Domain).
+"""
 
 setup(name="fred",
       version="1.0",
       description="St. Louis Federal Reserve FRED API",
-      long_description=read_file('README.md'),
+      long_description=long_description,
       keywords="fred, fred api, federal reserve, st. louis fed",
       author="Zach Williams",
       author_email="hey@zachwill.com",
